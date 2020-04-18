b8fd0984272b07f91de2c099fb1f5df9ba77b90d
Sebastian- <hmurgu@hotmail.com>
2020-04-17 17:32:08 -0700

    initial page components #12

---
+++ src/components/Welcome.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AssignmentIcon from "@material-ui/icons/Assignment";
+
+const useStyles = makeStyles({
+  page: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  welcome: {
+    marginTop: "50px",
+  },
+  checkIn: {
+    marginTop: "100px",
+  },
+});
+
+function Welcome() {
+  const classes = useStyles();
+  return (
+    <div className={classes.page}>
+      <Typography variant="h2" align="center" className={classes.welcome}>
+        Welcome To Our Clinic
+      </Typography>
+      <Button
+        size="large"
+        variant="contained"
+        color="primary"
+        className={classes.checkIn}
+        endIcon={<AssignmentIcon />}
+      >
+        <Typography variant="h5">Check In</Typography>
+      </Button>
+    </div>
+  );
+}
+
+export default Welcome;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Appointment from "./Appointment";
+import CheckIn from "./CheckIn";
+import Welcome from "./Welcome";
+import Layout from "./Layout";
+
+export { Appointment, CheckIn, Welcome, Layout };
