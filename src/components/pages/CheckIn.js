ed255545494b4fb9c35095e5faf1224f8ffd75fb
Sebastian- <hmurgu@hotmail.com>
2020-04-20 12:28:12 -0700

    add styled form with fields #18

--- src/components/pages/CheckIn.js
+++ src/components/pages/CheckIn.js
@@ -1,8 +1,58 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { TextField, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(2),
+  },
+  form: {
+    margin: "30px auto 0",
+    maxWidth: "700px",
+    "& div": {
+      display: "flex",
+      [theme.breakpoints.down("xs")]: {
+        flexDirection: "column",
+      },
+    },
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      flex: 1,
+    },
+  },
+}));
 
 function CheckIn() {
-  return <Typography>Check In form would go here</Typography>;
+  const styles = useStyles();
+  return (
+    <Paper className={styles.root}>
+      <Typography variant="h4" align="center">
+        Please Provide Your Information
+      </Typography>
+      <form className={styles.form}>
+        <div>
+          <TextField label="First Name"></TextField>
+          <TextField label="Last Name"></TextField>
+          <TextField
+            id="date"
+            label="Birthday"
+            type="date"
+            defaultValue="2017-05-24"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <TextField label="Street Address"></TextField>
+        <div>
+          <TextField label="City"></TextField>
+          <TextField label="Province"></TextField>
+          <TextField label="Postal Code"></TextField>
+        </div>
+      </form>
+    </Paper>
+  );
 }
 
 export default CheckIn;
