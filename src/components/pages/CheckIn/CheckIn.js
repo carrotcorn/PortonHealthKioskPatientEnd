b1e719220fcf50f4969481a06b760f825aeb023f
Sebastian- <hmurgu@hotmail.com>
2020-04-23 10:41:07 -0700

    create table with dummy appointment data #4

--- src/components/pages/CheckIn/CheckIn.js
+++ src/components/pages/CheckIn/CheckIn.js
@@ -1,17 +1,65 @@
-import React from "react";
-import { Paper, Typography } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  Paper,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TableContainer,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import CheckInForm from "./CheckInForm";
+import { format, addMinutes } from "date-fns";
+
+function createAppointment(id, firstName, lastInitial, attending) {
+  const randTime = addMinutes(Date.now(), Math.floor(Math.random() * 15 + 1));
+  const formattedTime = format(randTime, "p");
+  return { id, firstName, lastInitial, attending, time: formattedTime };
+}
 
 function CheckIn() {
   const styles = useStyles();
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    setAppointments([
+      createAppointment(1, "Abigail", "A.", "Walk In"),
+      createAppointment(2, "Lucas", "J.", "Dr. Johnson"),
+      createAppointment(3, "Thomas", "F.", "Dr. Armstrong"),
+      createAppointment(4, "Peter", "C.", "Dr. Squire"),
+      createAppointment(5, "Eric", "B.", "Dr. Smith"),
+      createAppointment(6, "Andrea", "F.", "Dr. Toledo"),
+    ]);
+  }, []);
 
   return (
     <Paper className={styles.root}>
       <Typography variant="h4" align="center">
-        Please Provide Your Information
+        Please Select an Upcoming Appointment
       </Typography>
-      <CheckInForm />
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Attending</TableCell>
+              <TableCell align="right">Time</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {appointments.map((appointment) => (
+              <TableRow key={appointment.id}>
+                <TableCell component="th" scope="row">
+                  {`${appointment.firstName} ${appointment.lastInitial}`}
+                </TableCell>
+                <TableCell align="right">{appointment.attending}</TableCell>
+                <TableCell align="right">{appointment.time}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 }
