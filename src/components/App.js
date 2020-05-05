d2ccdd0c9de7b0c155a6e77bf4dbba704e49e868
Sebastian- <hmurgu@hotmail.com>
2020-05-04 21:20:04 -0700

    integrate routing and context #9

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
-import { Welcome, CheckIn, Appointment } from "./pages";
+import { Welcome, CheckIn, Appointment, Login } from "./pages";
 import Layout from "./Layout";
+import { getCurrentUser } from "../util/API";
+import PrivateRoute from "../util/PrivateRoute";
+import { UserContext } from "./Contexts";
+import { CircularProgress } from "@material-ui/core";
 
 function App() {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await getCurrentUser();
+        if (response.success) {
+          console.log(response.result);
+          setUser(response.result);
+        }
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
   return (
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Welcome} />
-        <Route exact path="/checkin" component={CheckIn} />
-        <Route exact path="/appointment" component={Appointment} />
-      </Switch>
-    </Layout>
+    <UserContext.Provider value={{ user, setUser }}>
+      <Layout>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/">
+            <Welcome />
+          </PrivateRoute>
+          <PrivateRoute path="/checkin">
+            <CheckIn />
+          </PrivateRoute>
+          <PrivateRoute path="/appointment">
+            <Appointment />
+          </PrivateRoute>
+        </Switch>
+      </Layout>
+    </UserContext.Provider>
   );
 }
 
