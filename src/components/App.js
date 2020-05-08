6a2da764dcfccbb33aa14b40ef66c4764a63147a
Sebastian- <hmurgu@hotmail.com>
2020-05-08 09:19:18 -0700

    add route for invalid pages

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import { getCurrentUser } from "../util/API";
 import PrivateRoute from "../util/PrivateRoute";
 import { UserContext } from "./Contexts";
 import LoadingScreen from "../util/LoadingScreen";
+import PageNotFound from "../util/PageNotFound";
 
 function App() {
   const [user, setUser] = useState();
@@ -42,6 +43,7 @@ function App() {
           <PrivateRoute path="/confirmation">
             <Confirmation />
           </PrivateRoute>
+          <Route path="*" component={PageNotFound} />
         </Switch>
       </Layout>
     </UserContext.Provider>
